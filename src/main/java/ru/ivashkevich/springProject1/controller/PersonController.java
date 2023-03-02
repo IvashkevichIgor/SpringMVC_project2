de1626f0a440f25f3d2975318368d047307e77c5
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-03-02 15:04:15 +0300

    add patch and delete mapping

--- src/main/java/ru/ivashkevich/springProject1/controller/PersonController.java
+++ src/main/java/ru/ivashkevich/springProject1/controller/PersonController.java
@@ -48,4 +48,18 @@ public class PersonController {
         personDAO.save(person);
         return "redirect:/people";
     }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
+        if(bindingResult.hasErrors())
+            return "people/edit";
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
 }
