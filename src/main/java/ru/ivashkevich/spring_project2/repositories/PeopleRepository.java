0ccc40beb3c23f90919988604dc06eee1ccb3539
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-03-14 18:00:34 +0300

    add boolean method for PersonValidator

--- src/main/java/ru/ivashkevich/spring_project2/repositories/PeopleRepository.java
+++ src/main/java/ru/ivashkevich/spring_project2/repositories/PeopleRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.ivashkevich.spring_project2.models.Person;
 
+import java.util.List;
+
 @Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+    List<Person> findByName(String name);
+
+    boolean existsByName(String name);
 }
