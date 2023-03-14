67a915795e5897c20ed3836e848bc38265abdb5a
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-03-14 15:27:31 +0300

    change model classes into entities

--- src/main/java/ru/ivashkevich/spring_project2/models/Book.java
+++ src/main/java/ru/ivashkevich/spring_project2/models/Book.java
@@ -1,30 +1,42 @@
 package ru.ivashkevich.spring_project2.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
+@Entity
+@Table(name = "book")
 public class Book {
-    private int bookId;
-    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
     private String title;
     @NotEmpty(message = "Имя автора не должно быть пустым")
+    @Column(name = "author")
     private String author;
+    @Column(name = "publication_year")
     private int publicationYear;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
 
     public Book(){}
 
-    public Book(int bookId, String title, String author, int publicationYear) {
-        this.bookId = bookId;
+    public Book(String title, String author, int publicationYear) {
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
     }
 
-    public int getBookId() {
-        return bookId;
+    public int getId() {
+        return id;
     }
 
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -51,13 +63,22 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
-                "id=" + bookId +
+                "id=" + id +
                 ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", publicationYear=" + publicationYear +
+                ", owner=" + owner +
                 '}';
     }
 }
--- src/main/java/ru/ivashkevich/spring_project2/models/Person.java
+++ src/main/java/ru/ivashkevich/spring_project2/models/Person.java
@@ -1,32 +1,44 @@
 package ru.ivashkevich.spring_project2.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.List;
 
+@Entity
+@Table(name = "person")
 public class Person {
 
-    private int personId;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     @NotEmpty(message = "Имя не должно быть пустым")
     @Size(min = 5, max = 100, message = "Длина ФИО должна быть не менее 5 символов и не более 100")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 1900, message = "Год рождения должен быть больше чем 1900")
+    @Column(name = "birth_year")
     private int birthYear;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
     public Person(){}
 
-    public Person(int personId, String name, int birthYear) {
-        this.personId = personId;
+    public Person(String name, int birthYear) {
         this.name = name;
         this.birthYear = birthYear;
     }
 
-    public int getPersonId() {
-        return personId;
+    public int getId() {
+        return id;
     }
 
-    public void setPersonId(int personId) {
-        this.personId = personId;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -45,10 +57,18 @@ public class Person {
         this.birthYear = birthYear;
     }
 
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
-                "personId=" + personId +
+                "id=" + id +
                 ", name='" + name + '\'' +
                 ", birthYear=" + birthYear +
                 '}';
