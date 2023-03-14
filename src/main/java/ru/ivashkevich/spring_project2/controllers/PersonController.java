5d416403f0f23eb71201c49aa3492066d52c9558
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-03-14 18:25:52 +0300

    change PersonController, now it works with repository through PeopleService

--- src/main/java/ru/ivashkevich/spring_project2/controllers/PersonController.java
+++ src/main/java/ru/ivashkevich/spring_project2/controllers/PersonController.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.ivashkevich.spring_project2.dao.PersonDAO;
 import ru.ivashkevich.spring_project2.models.Person;
+import ru.ivashkevich.spring_project2.services.PeopleService;
 import ru.ivashkevich.spring_project2.util.PersonValidator;
 
 import javax.validation.Valid;
@@ -14,27 +14,27 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/people")
 public class PersonController {
-    private final PersonDAO personDAO;
+    private final PeopleService peopleService;
 
     private final PersonValidator personValidator;
 
     @Autowired
-    public PersonController(PersonDAO personDAO, PersonValidator personValidator) {
-        this.personDAO = personDAO;
+    public PersonController(PeopleService peopleService, PersonValidator personValidator) {
+        this.peopleService = peopleService;
         this.personValidator = personValidator;
     }
 
     @GetMapping
     public String showAllPeople(Model model){
-        model.addAttribute("people", personDAO.getAllPeople());
+        model.addAttribute("people", peopleService.getAllPeople());
 
         return "people/all-people";
     }
 
     @GetMapping("/{id}")
     public String showPerson(@PathVariable int id, Model model){
-        model.addAttribute("person", personDAO.getPersonById(id));
-        model.addAttribute("books", personDAO.getPersonBookList(id));
+        model.addAttribute("person", peopleService.getPersonById(id));
+        model.addAttribute("books", peopleService.getBooksByPersonId(id));
 
         return "people/person";
     }
@@ -47,7 +47,7 @@ public class PersonController {
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id){
-        model.addAttribute("person", personDAO.getPersonById(id));
+        model.addAttribute("person", peopleService.getPersonById(id));
         return "people/edit";
     }
 
@@ -58,23 +58,21 @@ public class PersonController {
         if (bindingResult.hasErrors())
             return "people/new";
 
-        personDAO.create(person);
+        peopleService.create(person);
         return "redirect:/people";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
-        personValidator.validate(person, bindingResult);
-
         if(bindingResult.hasErrors())
             return "people/edit";
-        personDAO.update(id, person);
+        peopleService.update(id, person);
         return "redirect:/people";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id){
-        personDAO.delete(id);
+        peopleService.delete(id);
         return "redirect:/people";
     }
 }
--- src/main/java/ru/ivashkevich/spring_project2/services/PeopleService.java
+++ src/main/java/ru/ivashkevich/spring_project2/services/PeopleService.java
@@ -1,12 +1,16 @@
 package ru.ivashkevich.spring_project2.services;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.ivashkevich.spring_project2.models.Book;
 import ru.ivashkevich.spring_project2.models.Person;
 import ru.ivashkevich.spring_project2.repositories.PeopleRepository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -27,8 +31,18 @@ public class PeopleService {
         return peopleRepository.findById(id).orElse(null);
     }
 
-    public Person getPersonByName(String name){
-        return peopleRepository.findByName(name);
+    public List<Book> getBooksByPersonId(int id){
+        Optional<Person> optional = peopleRepository.findById(id);
+        if (optional.isPresent()){
+            Hibernate.initialize(optional.get().getBooks());
+            return optional.get().getBooks();
+        }
+        else
+            return Collections.emptyList();
+    }
+
+    public boolean hasPersonWithName(String name){
+        return peopleRepository.existsByName(name);
     }
 
     @Transactional
