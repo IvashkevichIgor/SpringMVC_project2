dfe94e21a490d8958a733e5839eef343c69294f7
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-03-15 07:31:22 +0300

    add standard CRUD methods in service class

---
+++ src/main/java/ru/ivashkevich/spring_project2/services/BooksService.java
@@ -0,0 +1,45 @@
+package ru.ivashkevich.spring_project2.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.ivashkevich.spring_project2.models.Book;
+import ru.ivashkevich.spring_project2.repositories.BooksRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class BooksService {
+
+    private final BooksRepository booksRepository;
+
+    @Autowired
+    public BooksService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public List<Book> getAllBooks(){
+        return booksRepository.findAll();
+    }
+
+    public Book getBookById(int id){
+        return booksRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void create(Book book){
+        booksRepository.save(book);
+    }
+
+    @Transactional
+    public void update(int id, Book updatedBook){
+        updatedBook.setId(id);
+        booksRepository.save(updatedBook);
+    }
+
+    @Transactional
+    public void delete(int id){
+        booksRepository.deleteById(id);
+    }
+}
